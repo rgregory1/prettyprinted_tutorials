4b5185b1665b77de9d00d6289784092652bacf83
rusti <mrgregory1@gmail.com>
2019-01-10 10:02:22 -0500

    update for added row

--- flask_app.py
+++ flask_app.py
@@ -1,38 +1,67 @@
-from flask import Flask, g
+from flask import Flask, g, request, redirect, url_for, render_template
 import sqlite3
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'a random text string'
+app.config["DEBUG"] = True
+app.config["SECRET_KEY"] = "a random text string"
+
 
 def connect_db():
-    sql = sqlite3.connect('data.db')
+    sql = sqlite3.connect("data.db")
     sql.row_factory = sqlite3.Row
     return sql
 
+
 def get_db():
-    if not hasattr(g, 'sqlite3'):
+    if not hasattr(g, "sqlite3"):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+
 @app.teardown_appcontext
 def close_db(error):
-    if hasattr(g, 'sqlite_db'):
+    if hasattr(g, "sqlite_db"):
         g.sqlite_db.close()
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'this is the index'
+    return "yup"
 
-@app.route('/viewresults')
+
+@app.route("/home", methods=["POST", "GET"])
+def home():
+    db = get_db()
+    cur = db.execute("select id, name, location from users")
+    results = cur.fetchall()
+
+    return render_template("home.html", results=results)
+
+
+@app.route("/viewresults")
 def viewresults():
     db = get_db()
-    cur = db.execute('select id, name, location from users')
+    cur = db.execute("select id, name, location from users")
     results = cur.fetchall()
-    return '<h1>The ID is {}. The name is {}. The location is {}.</h1>'.format(results[0]['id'], results[0]['name'], results[0]['location'])
+    return "<h1>The ID is {}. The name is {}. The location is {}.</h1>".format(
+        results[2]["id"], results[2]["name"], results[2]["location"]
+    )
 
 
-if __name__ == '__main__':
+@app.route("/theform", methods=["GET", "POST"])
+def theform():
+    if request.method == "GET":
+        return render_template("form.html")
+    else:
+        name = request.form["name"]
+        location = request.form["location"]
+
+        db = get_db()
+        db.execute("insert into users (name, location) values (?, ?)", [name, location])
+        db.commit()
+        return redirect(url_for("theform"))
+
+
+if __name__ == "__main__":
     app.run()
